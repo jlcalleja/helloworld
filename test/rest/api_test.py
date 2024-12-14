1cf189a150272479ce99d61865473ee7f7cdd45a
Jose Luis Calleja <joseluiscallejadeol@gmail.com>
2024-12-14 13:27:53 +0100

    Update api_test.py

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -47,13 +47,17 @@ class TestApi(unittest.TestCase):
 
     def test_api_divide_by_zero(self):
         url = f"{BASE_URL}/calc/divide/6/0"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(
-            response.status, http.client.NOT_ACCEPTABLE, f"Error en la petición API a {url}"
-        )
-        self.assertIn(
-            "Error HTTP 406", response.read().decode(), "Error HTTP 406"
-        )
+        try:
+            response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+            self.assertEqual(
+                response.status, http.client.NOT_ACCEPTABLE, f"Error en la petición API a {url}"
+            )
+            self.assertIn(
+                "Error HTTP 406", response.read().decode(), "ERROR DIVIDE BY ZERO"
+            )
+        except urllib.error.HTTPError as e:
+            self.assertEqual(e.code, http.client.NOT_ACCEPTABLE)
+            self.assertIn("Error HTTP 406", e.read().decode(), "ERROR DIVIDE BY ZERO")
 
     def test_api_sqrt(self):
         url = f"{BASE_URL_MOCK}/calc/sqrt/64"
