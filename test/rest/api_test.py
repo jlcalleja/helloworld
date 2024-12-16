f2e897b08438fe65caff4fbe64d1aefc6c5e7973
Jose Luis Calleja <joseluiscallejadeol@gmail.com>
2024-12-16 11:08:23 +0100

    Update api_test.py

--- test/rest/api_test.py
+++ test/rest/api_test.py
@@ -2,6 +2,7 @@ import http.client
 import os
 import unittest
 from urllib.request import urlopen
+from urllib.error import HTTPError
 
 import pytest
 
@@ -25,6 +26,19 @@ class TestApi(unittest.TestCase):
             response.read().decode(), "3", "ERROR ADD"
         )
 
+    def test_api_sqrt(self):
+        url = f"{BASE_URL_MOCK}/calc/sqrt/64"
+        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        self.assertEqual(
+            response.status, http.client.OK, f"Error en la petición API a {url}"
+        )
+        self.assertEqual(
+            response.read().decode(), "8", "ERROR SQRT"
+        )
+
+
+    # Pruebas nuevas
+    
     def test_api_multiply(self):  # Asegúrate de que esta función no esté dentro de test_api_add
         url = f"{BASE_URL}/calc/multiply/2/3"
         response = urlopen(url, timeout=DEFAULT_TIMEOUT)
@@ -44,27 +58,23 @@ class TestApi(unittest.TestCase):
         self.assertEqual(
             response.read().decode(), "3.0", "ERROR DIVIDE"
         )
-
+        
     def test_api_divide_by_zero(self):
         url = f"{BASE_URL}/calc/divide/6/0"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(
-            response.status, http.client.OK, f"Error en la petición API a {url}"
-        )
-        self.assertEqual(
-            response.read().decode(), "Error HTTP 406", "ERROR DIVIDE BY ZERO"
-        )
+        try:
+            # Llamada a la API
+            response = urlopen(url, timeout=DEFAULT_TIMEOUT)
+        except HTTPError as e:
+            # Verifica que el código de estado sea 406
+            assert e.code == 406
+            
+            # Verifica que el mensaje de error sea el esperado
+            error_message = e.read().decode('utf-8')  # Lee el contenido del cuerpo de la respuesta
+            assert error_message == "Division by zero is not allowed"
+        else:
+            # Si no ocurre error, falla el test
+            pytest.fail("Expected HTTPError but got successful response")
 
-
-    def test_api_sqrt(self):
-        url = f"{BASE_URL_MOCK}/calc/sqrt/64"
-        response = urlopen(url, timeout=DEFAULT_TIMEOUT)
-        self.assertEqual(
-            response.status, http.client.OK, f"Error en la petición API a {url}"
-        )
-        self.assertEqual(
-            response.read().decode(), "8", "ERROR SQRT"
-        )
 
 if __name__ == "__main__":  # pragma: no cover
     unittest.main()
